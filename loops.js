c16eb1e56b662682988068d5ce90484f03c15fbc
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-05 23:23:42 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,11 +15,11 @@ function whileLoop(n) {
     }
     return "done"
     }
-    
+
     function maybeTrue() {
       return Math.random() >= 0.5
     }
-    
+
 function doWhileLoop(array) {
   do {
     array.shift()
