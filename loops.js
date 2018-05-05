0b63000e926d713d3496526eb89a772b578727f6
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-05 23:27:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -16,13 +16,10 @@ function whileLoop(n) {
     return "done"
     }
 
-    function maybeTrue() {
-      return Math.random() >= 0.5
-    }
-
 function doWhileLoop(array) {
   do {
     array.shift()
     }
    while (array.length > 0 && maybeTrue())
+   return array
 }
