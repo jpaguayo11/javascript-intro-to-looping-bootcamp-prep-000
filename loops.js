bdda6db0394f9108ec6cb7c574c5bdd1eaf0b2a8
Jose Aguayo <jpaguayo11@yahoo.es>
2018-05-05 23:22:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,7 +21,6 @@ function doWhileLoop(array) {
     function maybeTrue() {
       return Math.random() >= 0.5
     }
-    maybeTrue()
     array.shift()
     }
    while (array.length > 0 && maybeTrue())
